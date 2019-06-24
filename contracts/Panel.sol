b57c20863fca98f68006332daf16271975085bd0
biel <biel.mir@trivago.com>
2019-06-24 14:33:38 +0200

    adding panel contract

---
+++ contracts/Panel.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+contract Panel {
+
+    uint public controlPointCount;
+
+    struct ControlPoint {
+        uint id;
+        string desc;
+        uint temp;
+        bool isPositive;
+    }
+
+    mapping(uint => ControlPoint) public controlPoints;
+
+    struct Trigger {
+        bool positive;
+        uint temp;
+    }
+    constructor() public{
+        addControlPoint("con1",30,true);
+    }
+
+    function addControlPoint(string memory _desc, uint _temp,bool _isPositive) public {
+        controlPointCount++;
+        controlPoints[controlPointCount] = ControlPoint(controlPointCount,_desc,_temp,_isPositive);
+    }
+}
---
+++ migrations/2_deploy_panel.js
@@ -0,0 +1,5 @@
+const Panel = artifacts.require("Panel");
+
+module.exports = function(deployer) {
+    deployer.deploy(Panel);
+};
