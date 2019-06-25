c3095426c80038fac2cdd53d1122d14bebdd16a8
biel <biel.mir@trivago.com>
2019-06-25 17:20:27 +0200

    add some more test

--- test/panel.js
+++ test/panel.js
@@ -3,11 +3,26 @@ var Panel = artifacts.require("./Panel.sol");
 contract("Panel", function(accounts) {
     var electionInstance;
 
-    it("initializes with two control points", function () {
+    it("initializes with 1 control points", function () {
         return Panel.deployed().then(function (instance) {
             return instance.controlPointCount();
         }).then(function (count) {
             assert.equal(count, 1);
         });
     });
+
+    it("it initializes the points with correct data", function() {
+        return Panel.deployed().then(function (instance) {
+            panelInstance = instance;
+            return panelInstance.controlPoints(1);
+        }).then(function (controlPoint) {
+            assert.equal(controlPoint[0], 1, "correct id");
+            assert.equal(controlPoint[1], "con1", "correct name");
+            assert.equal(controlPoint[2], 30, "correct temp");
+            assert.equal(controlPoint[3], true, "correct value");
+
+        })
+    });
+
+
 });
